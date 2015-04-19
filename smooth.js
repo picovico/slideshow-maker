2d28377489b8475fc9dc5713273ac56780055451
acpmasquerade <d@picovico.com>
2015-04-20 00:25:10 +0545

    slideshow maker landing

---
+++ smooth.js
@@ -0,0 +1,31 @@
+  jQuery(document).ready(function(){ 
+ 
+        jQuery(window).scroll(function(){
+            if (jQuery(this).scrollTop() > 100) {
+                jQuery('.scrollup').fadeIn();
+            } else {
+                jQuery('.scrollup').fadeOut();
+            }
+        }); 
+ 
+        jQuery('.scrollup').click(function(){
+            jQuery("html, body").animate({ scrollTop: 0 }, 700);
+            return false;
+        });
+ 
+  jQuery(function() {
+  jQuery('a[href*=#]:not([href=#]), .max-hero a.button').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = jQuery(this.hash);
+      target = target.length ? target : jQuery('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        jQuery('html,body').animate({
+          scrollTop: target.offset().top
+        }, 700);
+        return false;
+      }
+    }
+  });
+});
+});
+
